f755603b895e4034ec885418da41ecd180ed6e88
Ashish Kumar <ashishkumar.98@outlook.com>
2024-04-28 08:38:24 +0530

    classesFixed

--- kmph-learning/src/components/pages/Blog.jsx
+++ kmph-learning/src/components/pages/Blog.jsx
@@ -5,7 +5,7 @@ import BlogCard from "../cards/BlogCard";
 const Blog = () => {
   return (
     <section className="flex flex-col  bg-purple-50 p-5 dark:bg-body-dark ">
-      <h1 className="section-heading mb-2 p-3  text-black">Coding Articles</h1>
+      <h1 className="section-heading  mb-2 p-3  text-black">Coding Articles</h1>
       <div>
         {codingBlogs.map((blog, index) => {
           return (

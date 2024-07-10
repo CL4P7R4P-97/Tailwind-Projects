b2a577d33e401c228fb30603287fa7194afde598
Ashish Kumar <ashish.kumar98@outlook.com>
2024-07-10 22:46:04 +0530

    imgFix

--- kmph-learning/src/components/sections/CoursesSection.jsx
+++ kmph-learning/src/components/sections/CoursesSection.jsx
@@ -6,26 +6,26 @@ const courses = [
     courseTitle: "Python Tutorials - 100 Days of Code",
     courseDescription:
       "Python is one of the most demanded programming languages in the job market. Surprisingly, it is equally easy to learn and master Python. Let's commit our 100 days of code to python",
-    imgUrl: "../../src/assets/images/courses/python.jpg",
+    imgUrl: "../../assets/images/courses/python.jpg",
   },
   {
     courseTitle: "Ultimate JavaScript Course",
     courseDescription:
       "This latest JavaScript course comes with premium curriculum that covers everything from basics to advance. On top of that, you will get my handwritten notes. Jump In now!",
-    imgUrl: "../../src/assets/images/courses/js.jpg",
+    imgUrl: "../../assets/images/courses/js.jpg",
   },
   {
     courseTitle: "React JS Tutorials For Beginners",
     courseDescription:
       "React is a free and open-source front-end JavaScript library. This series will cover React from starting to the end. We will learn react from the ground up!. All materials provided ",
-    imgUrl: "../../src/assets/images/courses/react.jpg",
+    imgUrl: "../../assets/images/courses/react.jpg",
   },
 ];
 const CoursesSection = () => {
   return (
     <section className="section-courses">
       <h1 className=" section-heading">Recommended Courses</h1>
-      <div className=" card:flex card:flex-row card:items-stretch card:justify-evenly card:space-x-8 items-center    p-10 xxsm:flex xxsm:flex-col   xxsm:justify-center xxsm:space-y-3">
+      <div className=" items-center p-10 xxsm:flex xxsm:flex-col xxsm:justify-center xxsm:space-y-3    card:flex card:flex-row card:items-stretch   card:justify-evenly card:space-x-8">
         {courses.map((course, index) => {
           return (
             <CourseCard
